040cdb05f637b5dfa3f41ad9a567e716a0d42ed5
RandomSoggyMonkee <143637006+RandomSoggyMonkee@users.noreply.github.com>
2023-10-26 15:31:53 +0100

    Swap style to css, display boxes in 16x16 grid

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function createBoxes() {
     let mainDiv = document.querySelector('#etchSketch');
-    for (let i = 0; i < 257; i++) {
+    for (let i = 0; i < 272; i++) {
         let box = document.createElement('div');
-        box.style = 'border: 1px solid black; height: 25px; width: 25px; margin: 0 3px 3px 0;';
         mainDiv.appendChild(box);
     }
 }
