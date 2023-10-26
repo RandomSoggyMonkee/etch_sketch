60327445f1d0a3db4fc808e02a66cba971e260e6
RandomSoggyMonkee <143637006+RandomSoggyMonkee@users.noreply.github.com>
2023-10-26 14:45:11 +0100

    Add styles

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function createBoxes() {
     let mainDiv = document.querySelector('#etchSketch');
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 257; i++) {
         let box = document.createElement('div');
+        box.style = 'border: 1px solid black; height: 25px; width: 25px; margin: 0 3px 3px 0;';
         mainDiv.appendChild(box);
     }
 }
