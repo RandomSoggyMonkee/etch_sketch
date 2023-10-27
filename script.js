86891ae2c923f134753470b2b742d00c1dc2a0c5
RandomSoggyMonkee <143637006+RandomSoggyMonkee@users.noreply.github.com>
2023-10-27 14:00:42 +0100

    Add class to boxes

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function createBoxes() {
     let mainDiv = document.querySelector('#etchSketch');
     for (let i = 0; i < 272; i++) {
         let box = document.createElement('div');
+        box.setAttribute('class', 'box')
         mainDiv.appendChild(box);
     }
 }
 
+
 createBoxes();
+
+let boxes = document.querySelectorAll('.box');
+
+boxes.forEach(function(item) {
+    item.addEventListener('mouseover', function() {
+        item.setAttribute('style', 'background-color: black');
+    });
+});
