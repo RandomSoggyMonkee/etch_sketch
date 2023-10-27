9c998a657a2a8fd1f8dedd8b68f8e5c13e76656b
RandomSoggyMonkee <143637006+RandomSoggyMonkee@users.noreply.github.com>
2023-10-27 18:39:04 +0100

    Add functions for new game button

--- script.js
+++ script.js
@@ -1,25 +1,79 @@
-function createBoxes() {
-    let mainDiv = document.querySelector('#etchSketch');
-    for (let i = 0; i < 272; i++) {
+function shrinkNthBoxes(arr, nth) {
+    for(let i = 0; i < arr.length; i += nth) {
+        divsToResize.push(arr[i + 1]);
+    };
+    divsToResize.forEach(function(item) {
+        item.setAttribute('style', 'width: 100%; height: 0px; border: none;')
+    });
+};
+
+function createBoxes(num) {
+    let size = num * num + num;
+    for (let i = 0; i < size; i++) {
         let box = document.createElement('div');
         box.setAttribute('class', 'box')
-        mainDiv.appendChild(box);
+        shrinkNthBoxes(boxes, gridSize);
+        gridContainer.appendChild(box);
     }
 }
 
-createBoxes();
+// function nthBoxesArray(arr, nth) {
+//     for(let i = 0; i < arr.length; i += nth) {
+//         divsToResize.push(arr[i + 1]);
+//     };
+// };
+
+// function shrinkBoxes() {
+//     divsToResize.forEach(function(item) {
+//         item.setAttribute('style', 'width: 100%; height: 0px; border: none;')
+//     });
+// };
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    };
+};
+
+function boxColouring() {
+    boxes.forEach(function(item) {
+        item.addEventListener('mouseover', function() {
+            item.setAttribute('style', 'background-color: black');
+        });
+    });
+};
+
+function NewGame() {
+    gridSize = +prompt('Please enter the side length of the square to generated.');
+    removeAllChildNodes(gridContainer);
+    createBoxes(gridSize);
+    boxes = document.querySelectorAll('.box')
+    shrinkNthBoxes(boxes, gridSize);
+    boxColouring();
+};
+
+
+let gridContainer = document.querySelector('#etchSketch');
+
+let gridSize = '';
+
+let divsToResize = [];
 
 let boxes = document.querySelectorAll('.box');
 
-boxes.forEach(function(item) {
-    item.addEventListener('mouseover', function() {
-        item.setAttribute('style', 'background-color: black');
-    });
-});
 
-resetBtn = document.querySelector('#reset');
+let resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click', function() {
     boxes.forEach(function(item) {
         item.setAttribute('style', 'background-color: white');
         });
 });
+
+let newGameBtn = document.querySelector('#newGame');
+newGameBtn.addEventListener('click', newGame);
+
+
+
+createBoxes(16);
+
+boxColouring();
